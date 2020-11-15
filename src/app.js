7bccb0d5c5532ebc186a6f80d473622edd2c5c1b
Maycon Rogerio Tenorio de Albuquerque <mayconrt@ciandt.com>
2020-11-15 16:43:55 -0300

    Change app.js and add app.yaml

--- src/app.js
+++ src/app.js
@@ -28,4 +28,12 @@ app.use((error, req, res, next) => {
     })
 })
 
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
+
 module.exports = app
