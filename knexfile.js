901966545ce8ddc0a81d4c1bc2ccec1c2b906f94
Maycon Rogerio Tenorio de Albuquerque <mayconrt@ciandt.com>
2021-01-01 14:22:52 -0300

    finish config conect postgres heroku

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,22 @@
 module.exports = {
-  // development: {
-  //   client: 'mysql',
-  //   connection: 'mysql://b013fe1c605cb9:26a9becd@us-cdbr-east-02.cleardb.com/heroku_d71fda90a83dacf?reconnect=true',
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     directory: "./src/database/migrations"
-  //   },
-  //   useNullAsDefault: true,
-  // },    
+  development: {
+    client: "pg",
+    connection: {
+      connectionString:process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      }
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./src/database/migrations"
+    },
+    useNullAsDefault: true,
+  },
+
   // development: {
   //   client: "postgres",
   //   connection: {
@@ -26,41 +32,6 @@ module.exports = {
   //     directory: "./src/database/migrations"
   //   },
   //   useNullAsDefault: true,
-  // },
-
-  development: {
-    client: "pg",
-    connection: {
-      host:"ec2-52-6-75-198.compute-1.amazonaws.com",
-      port:5432,
-      dbname:"d9smcdchfn5qlo",
-      user: "nwfgqdtcsjlcvw",
-      password: "eeaa8b69dc6ca21c0e8168f6161ae570e215624034d5875455d8c85628e0b65e",
-      sslmode:"require",
-      rejectUnauthorized: false
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: "./src/database/migrations"
-    },
-    useNullAsDefault: true,
-  },
-
-  // development: {
-  //   client: 'postgres',
-  //   connection: "postgres://nwfgqdtcsjlcvw:eeaa8b69dc6ca21c0e8168f6161ae570e215624034d5875455d8c85628e0b65e@ec2-52-6-75-198.compute-1.amazonaws.com:5432/d9smcdchfn5qlo",    
-  //   rejectUnauthorized: false,
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     directory: "./src/database/migrations"
-  //   },
-  //   useNullAsDefault: true,
-  // }
+  // },  
 
 }   
