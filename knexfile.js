1cafe5ad13a4a6aca01eff222ea45a66b0863436
Maycon Rogerio Tenorio de Albuquerque <mayconrt@ciandt.com>
2020-12-31 23:01:12 -0300

    config heroku

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,18 @@
 module.exports = {
 
+  development: {
+    client: 'mysql',
+    connection: 'mysql://b013fe1c605cb9:26a9becd@us-cdbr-east-02.cleardb.com/heroku_d71fda90a83dacf?reconnect=true',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./src/database/migrations"
+    },
+    useNullAsDefault: true,
+  }    
+
   // development: {
   //   client: "postgres",
   //   connection: {
@@ -39,7 +52,7 @@ module.exports = {
 
   // development: {
   //   client: 'postgres',
-  //   connection: "postgres://nwfgqdtcsjlcvw:eeaa8b69dc6ca21c0e8168f6161ae570e215624034d5875455d8c85628e0b65e@ec2-52-6-75-198.compute-1.amazonaws.com:5432/d9smcdchfn5qlo?reconnect=true",    
+  //   connection: "postgres://nwfgqdtcsjlcvw:eeaa8b69dc6ca21c0e8168f6161ae570e215624034d5875455d8c85628e0b65e@ec2-52-6-75-198.compute-1.amazonaws.com:5432/d9smcdchfn5qlo",    
   //   pool: {
   //     min: 2,
   //     max: 10
@@ -49,18 +62,5 @@ module.exports = {
   //   },
   //   useNullAsDefault: true,
   // },
-
-  development: {
-    client: 'mysql',
-    connection: 'mysql://b013fe1c605cb9:26a9becd@us-cdbr-east-02.cleardb.com/heroku_d71fda90a83dacf?reconnect=true',
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: "./src/database/migrations"
-    },
-    useNullAsDefault: true,
-  }  
 
 }   
