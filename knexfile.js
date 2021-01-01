c2ea830c0a51f8d10f8b35ad7f2e4510fcd55e13
Maycon Rogerio Tenorio de Albuquerque <mayconrt@ciandt.com>
2021-01-01 13:19:21 -0300

    config heroku

--- knexfile.js
+++ knexfile.js
@@ -28,31 +28,17 @@ module.exports = {
   //   useNullAsDefault: true,
   // },
 
-  // development: {
-  //   client: "pg",
-  //   connection: {
-  //     host:"ec2-52-6-75-198.compute-1.amazonaws.com",
-  //     port:5432,
-  //     dbname:"d9smcdchfn5qlo",
-  //     user: "nwfgqdtcsjlcvw",
-  //     password: "eeaa8b69dc6ca21c0e8168f6161ae570e215624034d5875455d8c85628e0b65e",
-  //     sslmode:"require",
-  //     rejectUnauthorized: false
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     directory: "./src/database/migrations"
-  //   },
-  //   useNullAsDefault: true,
-  // },
-
   development: {
-    client: 'postgres',
-    connection: "postgres://nwfgqdtcsjlcvw:eeaa8b69dc6ca21c0e8168f6161ae570e215624034d5875455d8c85628e0b65e@ec2-52-6-75-198.compute-1.amazonaws.com:5432/d9smcdchfn5qlo",    
-    rejectUnauthorized: false,
+    client: "pg",
+    connection: {
+      host:"ec2-52-6-75-198.compute-1.amazonaws.com",
+      port:5432,
+      dbname:"d9smcdchfn5qlo",
+      user: "nwfgqdtcsjlcvw",
+      password: "eeaa8b69dc6ca21c0e8168f6161ae570e215624034d5875455d8c85628e0b65e",
+      sslmode:"require",
+      rejectUnauthorized: false
+    },
     pool: {
       min: 2,
       max: 10
@@ -61,6 +47,20 @@ module.exports = {
       directory: "./src/database/migrations"
     },
     useNullAsDefault: true,
-  }
+  },
+
+  // development: {
+  //   client: 'postgres',
+  //   connection: "postgres://nwfgqdtcsjlcvw:eeaa8b69dc6ca21c0e8168f6161ae570e215624034d5875455d8c85628e0b65e@ec2-52-6-75-198.compute-1.amazonaws.com:5432/d9smcdchfn5qlo",    
+  //   rejectUnauthorized: false,
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     directory: "./src/database/migrations"
+  //   },
+  //   useNullAsDefault: true,
+  // }
 
 }   
