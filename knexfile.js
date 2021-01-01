b1f998a5ef763f455645bf6950ffd37dce4c3e70
Maycon Rogerio Tenorio de Albuquerque <mayconrt@ciandt.com>
2021-01-01 09:27:25 -0300

    config heroku

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,16 @@
 module.exports = {
-  development: {
-    client: 'mysql',
-    connection: 'mysql://b013fe1c605cb9:26a9becd@us-cdbr-east-02.cleardb.com/heroku_d71fda90a83dacf?reconnect=true',
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: "./src/database/migrations"
-    },
-    useNullAsDefault: true,
-  }    
+  // development: {
+  //   client: 'mysql',
+  //   connection: 'mysql://b013fe1c605cb9:26a9becd@us-cdbr-east-02.cleardb.com/heroku_d71fda90a83dacf?reconnect=true',
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     directory: "./src/database/migrations"
+  //   },
+  //   useNullAsDefault: true,
+  // },    
   // development: {
   //   client: "postgres",
   //   connection: {
@@ -28,25 +28,25 @@ module.exports = {
   //   useNullAsDefault: true,
   // },
 
-  // development: {
-  //   client: "pg",
-  //   connection: {
-  //     host:"ec2-52-6-75-198.compute-1.amazonaws.com",
-  //     port:5432,
-  //     dbname:"d9smcdchfn5qlo",
-  //     user: "nwfgqdtcsjlcvw",
-  //     password: "eeaa8b69dc6ca21c0e8168f6161ae570e215624034d5875455d8c85628e0b65e",
-  //     sslmode:"require"
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     directory: "./src/database/migrations"
-  //   },
-  //   useNullAsDefault: true,
-  // },
+  development: {
+    client: "pg",
+    connection: {
+      host:"ec2-52-6-75-198.compute-1.amazonaws.com",
+      port:5432,
+      dbname:"d9smcdchfn5qlo",
+      user: "nwfgqdtcsjlcvw",
+      password: "eeaa8b69dc6ca21c0e8168f6161ae570e215624034d5875455d8c85628e0b65e",
+      sslmode:"require"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./src/database/migrations"
+    },
+    useNullAsDefault: true,
+  },
 
   // development: {
   //   client: 'postgres',
